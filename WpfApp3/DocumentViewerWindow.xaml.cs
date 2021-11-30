69a349854fb1a2560cb2b6018a3d870533f03d23
davgo0103 <davvgo@gmail.com>
2021-11-30 08:47:51 +0800

    修改

--- WpfApp3/DocumentViewerWindow.xaml.cs
+++ WpfApp3/DocumentViewerWindow.xaml.cs
@@ -10,25 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace WpfApp3
 {
     /// <summary>
-    /// MainWindow.xaml 的互動邏輯
+    /// DocumentViewerWindow.xaml 的互動邏輯
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class DocumentViewerWindow : Window
     {
-        public MainWindow()
+        public DocumentViewerWindow()
         {
-            
             InitializeComponent();
-        }
-
-        private void OpenButton_Click(object sender, RoutedEventArgs e)
-        {
-
         }
     }
 }
